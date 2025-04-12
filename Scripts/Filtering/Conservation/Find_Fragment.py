5118983ed4a22d9c644550c6f6b9eb78781f6d13
AnnaFriel <friel-a11@ulster.ac.uk>
2025-04-12 09:11:28 +0100

    Update Find_Fragment.py

--- Scripts/Filtering/Conservation/Find_Fragment.py
+++ Scripts/Filtering/Conservation/Find_Fragment.py
@@ -1,5 +1,5 @@
+#If the term fragment is found the sequence is removed
 file1=open("Uniprot_Bufavirus-1.fasta").readlines()
-#file2=open("uniprotkb_Cutavirus-1.fasta").readlines()
 File2=open("Fragment_Free_Bufavirus.fasta", "w")
 not_fragment=[]
 Fragments = []
@@ -11,27 +11,11 @@ for i in file1:
 		if "(Fragment)" in i:
 			Fragments.append(i)
 			nextline = False
-                       #print Fragments			nextline = False
 		else:
 			not_fragment.append(i)
 			nextline =True
 			File2.write(i)
 	elif nextline:
 		File2.write(i)
-#		if nextline:
-#			if ">" in i:
-#				nextline = False
-#			else:
-#				result.append(i.strip())
-#		 
-#		if "(Fragment)" not in i:
-#			nextline = True	
-#print result
-#File2.writelines(not_fragment)
-#File2.writelines(result)
-#	elif nextline:
-#		result.append(not_fragment
-#		result.append(i)
-#		nextline = False
-#		File2.writelines(result)
+
 
