c11d4b549e73ad3f850330b4bb2a4bc3b9a610db
AnnaFriel <friel-a11@ulster.ac.uk>
2025-04-12 08:49:37 +0100

    Update Remove_X.py

--- Remove_X.py
+++ Remove_X.py
@@ -1,7 +1,1 @@
-file1=open("filtering_consensus_epitope_output.txt", "r").readlines()
-file2=open("X_filtered_consensus_epitope_output.txt","w")
-Sequence_X= ""
-for i in file1:
-	if i[2]=="X":
-		Sequence_X=i[2]
-		file2.write(Sequence_X)
+#Find_X.py used instead
